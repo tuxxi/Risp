bb5b87357828039bc49064621bdfd3c9c65e80a5
Aidan Sojourner <aidan.sojourner@gmail.com>
2020-02-21 22:19:35 -0800

    Implement a basic REPL that just prints whatever the user typed.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+extern crate clap;
+
+use std::io;
+use std::io::prelude::*;
+
+fn main() -> io::Result<()> {
+    println!("risp v.{}", clap::crate_version!());
+    println!("Press Ctrl-C to exit");
+    println!();
+    repl()?;
+    Ok(())
+}
+
+fn repl() -> io::Result<()> {
+    loop { 
+        print!("risp > ");
+        io::stdout().flush()?;
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        
+        println!("  {}", input.trim());
+    }
 }
