dc40d048afca2b576a6ef9db79e706935845bb04
Aidan Sojourner <aidan.sojourner@gmail.com>
2020-02-21 22:26:46 -0800

    Added readline support to REPL.
    Now we get a command history, as well as readline editing support!

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,44 @@
 extern crate clap;
+extern crate rustyline;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 use std::io;
-use std::io::prelude::*;
 
 fn main() -> io::Result<()> {
     println!("risp v.{}", clap::crate_version!());
-    println!("Press Ctrl-C to exit");
     println!();
     repl()?;
     Ok(())
 }
 
 fn repl() -> io::Result<()> {
+    let mut rl = Editor::<()>::new();
+    // if rl.load_history("history.txt").is_err() {
+    //     println!("No previous history.");
+    // }
     loop { 
-        print!("risp > ");
-        io::stdout().flush()?;
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
-        
-        println!("  {}", input.trim());
+        let readline = rl.readline("risp > ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("  {}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
     }
+    // rl.save_history("history.txt").unwrap();
+    Ok(())
 }
